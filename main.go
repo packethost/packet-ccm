5713fc689866324e8e414035ea57d0b048aba3d4
Marques Johansson <mjohansson@equinix.com>
2021-06-29 19:53:51 -0400

    refactor config loading for --cloud-config
    Signed-off-by: Marques Johansson <mjohansson@equinix.com>

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"encoding/json"
 	goflag "flag"
 	"fmt"
-	"io/ioutil"
 	"math/rand"
 	"os"
-	"strconv"
 	"time"
 
-	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/util/wait"
+	cloudprovider "k8s.io/cloud-provider"
 	"k8s.io/cloud-provider/app"
+	cloudcontrollerconfig "k8s.io/cloud-provider/app/config"
+	"k8s.io/cloud-provider/options"
 	cliflag "k8s.io/component-base/cli/flag"
 	"k8s.io/component-base/logs"
 	_ "k8s.io/component-base/metrics/prometheus/clientgo" // for client metric registration
@@ -22,59 +22,29 @@ import (
 	"github.com/spf13/pflag"
 )
 
-const (
-	apiKeyName                   = "METAL_API_KEY"
-	projectIDName                = "METAL_PROJECT_ID"
-	facilityName                 = "METAL_FACILITY_NAME"
-	loadBalancerSettingName      = "METAL_LB"
-	envVarLocalASN               = "METAL_LOCAL_ASN"
-	envVarBGPPass                = "METAL_BGP_PASS"
-	envVarAnnotationLocalASN     = "METAL_ANNOTATION_LOCAL_ASN"
-	envVarAnnotationPeerASNs     = "METAL_ANNOTATION_PEER_ASNS"
-	envVarAnnotationPeerIPs      = "METAL_ANNOTATION_PEER_IPS"
-	envVarAnnotationSrcIP        = "METAL_ANNOTATION_SRC_IP"
-	envVarAnnotationBGPPass      = "METAL_ANNOTATION_BGP_PASS"
-	envVarEIPTag                 = "METAL_EIP_TAG"
-	envVarAPIServerPort          = "METAL_API_SERVER_PORT"
-	envVarBGPNodeSelector        = "METAL_BGP_NODE_SELECTOR"
-	defaultLoadBalancerConfigMap = "metallb-system:config"
-)
-
-var (
-	providerConfig string
-)
-
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 
-	command := app.NewCloudControllerManagerCommand()
+	opts, err := options.NewCloudControllerManagerOptions()
+	opts.KubeCloudShared.CloudProvider.Name = metal.ProviderName
 
+	if err != nil {
+		klog.Fatalf("unable to initialize command options: %v", err)
+	}
+	controllerInitializers := app.DefaultInitFuncConstructors
+	// remove unneeded controllers
+	delete(controllerInitializers, "route")
+	fss := cliflag.NamedFlagSets{
+		NormalizeNameFunc: cliflag.WordSepNormalizeFunc,
+	}
+	// TODO: how do we alias cloud-config (in the "generic" flagset) as provider-config, or offer a secondary (legacy) argument
 	pflag.CommandLine.SetNormalizeFunc(cliflag.WordSepNormalizeFunc)
 	pflag.CommandLine.AddGoFlagSet(goflag.CommandLine)
 
-	// add our config
-	command.PersistentFlags().StringVar(&providerConfig, "provider-config", "", "path to provider config file")
+	command := app.NewCloudControllerManagerCommand(opts, cloudInitializer, controllerInitializers, fss, wait.NeverStop)
 
 	logs.InitLogs()
 	defer logs.FlushLogs()
-
-	// parse our flags so we get the providerConfig
-	command.ParseFlags(os.Args[1:])
-
-	// register the provider
-	config, err := getMetalConfig(providerConfig)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "provider config error: %v\n", err)
-		os.Exit(1)
-	}
-	// report the config
-	printMetalConfig(config)
-
-	// register the provider
-	if err := metal.InitializeProvider(config); err != nil {
-		fmt.Fprintf(os.Stderr, "provider initialization error: %v\n", err)
-		os.Exit(1)
-	}
 
 	if err := command.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
@@ -82,154 +52,24 @@ func main() {
 	}
 }
 
-func getMetalConfig(providerConfig string) (metal.Config, error) {
-	// get our token and project
-	var config, rawConfig metal.Config
-	if providerConfig != "" {
-		configBytes, err := ioutil.ReadFile(providerConfig)
-		if err != nil {
-			return config, fmt.Errorf("failed to get read configuration file at path %s: %v", providerConfig, err)
+func cloudInitializer(config *cloudcontrollerconfig.CompletedConfig) cloudprovider.Interface {
+	cloudConfig := config.ComponentConfig.KubeCloudShared.CloudProvider
+	// initialize cloud provider with the cloud provider name and config file provided
+	cloud, err := cloudprovider.InitCloudProvider(cloudConfig.Name, cloudConfig.CloudConfigFile)
+	if err != nil {
+		klog.Fatalf("Cloud provider could not be initialized: %v", err)
+	}
+	if cloud == nil {
+		klog.Fatalf("Cloud provider is nil")
+	}
+
+	if !cloud.HasClusterID() {
+		if config.ComponentConfig.KubeCloudShared.AllowUntaggedCloud {
+			klog.Warning("detected a cluster without a ClusterID.  A ClusterID will be required in the future.  Please tag your cluster to avoid any future issues")
+		} else {
+			klog.Fatalf("no ClusterID found.  A ClusterID is required for the cloud provider to function properly.  This check can be bypassed by setting the allow-untagged-cloud option")
 		}
-		err = json.Unmarshal(configBytes, &rawConfig)
-		if err != nil {
-			return config, fmt.Errorf("failed to process json of configuration file at path %s: %v", providerConfig, err)
-		}
 	}
 
-	// read env vars; if not set, use rawConfig
-	apiToken := os.Getenv(apiKeyName)
-	if apiToken == "" {
-		apiToken = rawConfig.AuthToken
-	}
-	config.AuthToken = apiToken
-
-	projectID := os.Getenv(projectIDName)
-	if projectID == "" {
-		projectID = rawConfig.ProjectID
-	}
-	config.ProjectID = projectID
-
-	loadBalancerSetting := os.Getenv(loadBalancerSettingName)
-	config.LoadBalancerSetting = rawConfig.LoadBalancerSetting
-	// rule for processing: any setting in env var overrides setting from file
-	if loadBalancerSetting != "" {
-		config.LoadBalancerSetting = loadBalancerSetting
-	}
-	// and set for default
-	if config.LoadBalancerSetting == "" {
-		config.LoadBalancerSetting = defaultLoadBalancerConfigMap
-	}
-
-	facility := os.Getenv(facilityName)
-	if facility == "" {
-		facility = rawConfig.Facility
-	}
-
-	if apiToken == "" {
-		return config, fmt.Errorf("environment variable %q is required", apiKeyName)
-	}
-
-	if projectID == "" {
-		return config, fmt.Errorf("environment variable %q is required", projectIDName)
-	}
-
-	// if facility was not defined, retrieve it from our metadata
-	if facility == "" {
-		metadata, err := metal.GetAndParseMetadata("")
-		if err != nil {
-			return config, fmt.Errorf("facility not set in environment variable %q or config file, and error reading metadata: %v", facilityName, err)
-		}
-		facility = metadata.Facility
-	}
-	config.Facility = facility
-
-	// get the local ASN
-	localASN := os.Getenv(envVarLocalASN)
-	switch {
-	case localASN != "":
-		localASNNo, err := strconv.Atoi(localASN)
-		if err != nil {
-			return config, fmt.Errorf("env var %s must be a number, was %s: %v", envVarLocalASN, localASN, err)
-		}
-		config.LocalASN = localASNNo
-	case rawConfig.LocalASN != 0:
-		config.LocalASN = rawConfig.LocalASN
-	default:
-		config.LocalASN = metal.DefaultLocalASN
-	}
-
-	bgpPass := os.Getenv(envVarBGPPass)
-	if bgpPass != "" {
-		config.BGPPass = bgpPass
-	}
-
-	// set the annotations
-	config.AnnotationLocalASN = metal.DefaultAnnotationNodeASN
-	annotationLocalASN := os.Getenv(envVarAnnotationLocalASN)
-	if annotationLocalASN != "" {
-		config.AnnotationLocalASN = annotationLocalASN
-	}
-	config.AnnotationPeerASNs = metal.DefaultAnnotationPeerASNs
-	annotationPeerASNs := os.Getenv(envVarAnnotationPeerASNs)
-	if annotationPeerASNs != "" {
-		config.AnnotationPeerASNs = annotationPeerASNs
-	}
-	config.AnnotationPeerIPs = metal.DefaultAnnotationPeerIPs
-	annotationPeerIPs := os.Getenv(envVarAnnotationPeerIPs)
-	if annotationPeerIPs != "" {
-		config.AnnotationPeerIPs = annotationPeerIPs
-	}
-	config.AnnotationSrcIP = metal.DefaultAnnotationSrcIP
-	annotationSrcIP := os.Getenv(envVarAnnotationSrcIP)
-	if annotationSrcIP != "" {
-		config.AnnotationSrcIP = annotationSrcIP
-	}
-
-	config.AnnotationBGPPass = metal.DefaultAnnotationBGPPass
-	annotationBGPPass := os.Getenv(envVarAnnotationBGPPass)
-	if annotationBGPPass != "" {
-		config.AnnotationBGPPass = annotationBGPPass
-	}
-
-	if rawConfig.EIPTag != "" {
-		config.EIPTag = rawConfig.EIPTag
-	}
-	eipTag := os.Getenv(envVarEIPTag)
-	if eipTag != "" {
-		config.EIPTag = eipTag
-	}
-
-	apiServer := os.Getenv(envVarAPIServerPort)
-	switch {
-	case apiServer != "":
-		apiServerNo, err := strconv.Atoi(apiServer)
-		if err != nil {
-			return config, fmt.Errorf("env var %s must be a number, was %s: %v", envVarAPIServerPort, apiServer, err)
-		}
-		config.APIServerPort = int32(apiServerNo)
-	case rawConfig.APIServerPort != 0:
-		config.APIServerPort = rawConfig.APIServerPort
-	default:
-		// if nothing else set it, we set it to 0, to indicate that it should use whatever the kube-apiserver port is
-		config.APIServerPort = 0
-	}
-
-	config.BGPNodeSelector = rawConfig.BGPNodeSelector
-	if v := os.Getenv(envVarBGPNodeSelector); v != "" {
-		config.BGPNodeSelector = v
-	}
-
-	if _, err := labels.Parse(config.BGPNodeSelector); err != nil {
-		return config, fmt.Errorf("BGP Node Selector must be valid Kubernetes selector: %w", err)
-	}
-
-	return config, nil
-}
-
-// printMetalConfig report the config to startup logs
-func printMetalConfig(config metal.Config) {
-	lines := config.Strings()
-	for _, l := range lines {
-		klog.Infof(l)
-	}
+	return cloud
 }
